71c030a1ead679261616a227acbc6f017290391e
DuckDuckDown <DuckDUckDown@outlook.com>
2021-08-25 21:31:04 -0400

    End of Section 2

---
+++ API/Controllers/BaseAPIController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]   //Api/Activities
+    public class BaseAPIController : ControllerBase
+    {
+        
+    }
+}
---
+++ Persistance/Migrations/20210826010035_InitialCreate_1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistance.Migrations
+{
+    public partial class InitialCreate_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descriptin",
+                table: "Activities",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Activities",
+                newName: "Descriptin");
+        }
+    }
+}
